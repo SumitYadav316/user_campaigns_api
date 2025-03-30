e226c80b930aef3c5efc99fcba03b74b262663b5
sumitydavna@gmail.com <sumitydavna@gmail.com>
2025-03-30 23:26:36 +0530

    second

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create!([
+  { name: "John", email: "john1@gmail.com", campaigns_list: [{ "campaign_name": "cam1", "campaign_id": "id1" }, { "campaign_name": "cam2", "campaign_id": "id2" }] },
+  { name: "Jane", email: "jane2@gmail.com", campaigns_list: [{ "campaign_name": "cam1", "campaign_id": "id1" }, { "campaign_name": "cam3", "campaign_id": "id3" }] }
+])
